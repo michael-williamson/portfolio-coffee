e3dd8463fccda690796afb29f4558888f9565bd0
michael-williamson <michaeldavid1721@yahoo.com>
2020-07-24 17:35:03 -0500

    added more animation to mainbanner shapes, icons to single project page

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import '../style/MainBanner.css';
 
-import {Link} from 'react-router-dom';
-
 
 class MainBanner extends Component {
     render() {
-        return (
+        return (  
             <div className="main-banner container-fluid">
                 <div className="name-title-div">
                     <div className="name-title">Michael Williamson</div>
                 </div> 
                 <div className="h4">Web Developer</div> 
                 {/* <div className="bg-secondary text-white lead">On the road to artistry</div> */}
-                    <div className="mainSquare">
-                        <div className="shapeOne"></div>
-                        <div className="shapeTwo"></div>
-                    </div>
+                <div className="shapeContainer">
+                    <div className="mainSquare"></div>
+                    <div className="shapeOne"></div>
+                    <div className="shapeTwo"></div>
+
+                </div>
             </div>
         );
     }
